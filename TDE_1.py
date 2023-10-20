08bf6746c92a84ad796d44cae12137b159a16325
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2023-10-20 19:15:03 -0300

    Adiciona implicação

--- TDE_1.py
+++ TDE_1.py
@@ -5,26 +5,43 @@
 # p -> q and q -> ~q
 # (~p or q )and (~p or q)
 
+entrada = "p -> q"
+entradaUsuario = entrada.split(" ")
 
-t = [["V", "V", "F", "F"],
-     ["V", "F", "F", "V"], 
-     ["F", "V", "V", "F"],
-     ["F", "F", "V", "V"]]
-
-entradaUsuario = "p^q"
     
-def imprimeTabela(t, entradaUsuario):
+def imprimeTabela(entradaUsuario):
+    t = [["V", "V", "F", "F"],
+        ["V", "F", "F", "V"], 
+        ["F", "V", "V", "F"],
+        ["F", "F", "V", "V"]]
+        
+    tabela = [[f"|  {entradaUsuario[0]} ", f"|  {entradaUsuario[2]} ", f"|  ~{entradaUsuario[0]} ", f"|  ~{entradaUsuario[2]}  |"], \
+              [f"|  {t[0][0]} ", f"|  {t[0][1]} ", f"|  {t[0][2]}  ", f"|  {t[0][3]}   |"], \
+              [f"|  {t[1][0]} ", f"|  {t[1][1]} ", f"|  {t[1][2]}  ", f"|  {t[1][3]}   |"], \
+              [f"|  {t[2][0]} ", f"|  {t[2][1]} ", f"|  {t[2][2]}  ", f"|  {t[2][3]}   |"], \
+              [f"|  {t[3][0]} ", f"|  {t[3][1]} ", f"|  {t[3][2]}  ", f"|  {t[3][3]}   |"]]
+    return tabela
 
-    tabela = [[f"|   {entradaUsuario[0]}  ", f"|   {entradaUsuario[2]}  ", f"|  ~{entradaUsuario[0]}  ", f"|  ~{entradaUsuario[2]}   |"], \
-              [f"|   {t[0][0]}  ", f"|   {t[0][1]}  ", f"|   {t[0][2]}  ", f"|   {t[0][3]}   |"], \
-              [f"|   {t[1][0]}  ", f"|   {t[1][1]}  ", f"|   {t[1][2]}  ", f"|   {t[1][3]}   |"], \
-              [f"|   {t[2][0]}  ", f"|   {t[2][1]}  ", f"|   {t[2][2]}  ", f"|   {t[2][3]}   |"], \
-              [f"|   {t[3][0]}  ", f"|   {t[3][1]}  ", f"|   {t[3][2]}  ", f"|   {t[3][3]}   |"]]
+def implicacao(tabela, entrada):
+    implicacao = [f"  {entrada}  |", "    V     |","    F     |","    V     |","    V     |"]
 
-    for i in range(0,5):
-        print( tabela[i][0],tabela[i][1], tabela[i][2], tabela[i][3] )
+    return implicacao
+
+tabela = imprimeTabela(entradaUsuario)
+
+implicacao = implicacao(tabela, entrada)
+
+for i in range(0,5):
+    print( tabela[i][0],tabela[i][1], tabela[i][2], tabela[i][3], implicacao[i]) 
 
 
-imprimeTabela(t, entradaUsuario)
+
+
+
+
+
+
+
+
 
 
