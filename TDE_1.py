ce164f34afa2b0c864365b630d03b30e0e31c97b
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2023-10-20 19:41:20 -0300

    Adiciona as outras funções

--- TDE_1.py
+++ TDE_1.py
@@ -5,15 +5,14 @@
 # p -> q and q -> ~q
 # (~p or q )and (~p or q)
 
-entrada = "p -> q"
+entrada = "p <-> q"
 entradaUsuario = entrada.split(" ")
-
     
 def imprimeTabela(entradaUsuario):
     t = [["V", "V", "F", "F"],
-        ["V", "F", "F", "V"], 
-        ["F", "V", "V", "F"],
-        ["F", "F", "V", "V"]]
+         ["V", "F", "F", "V"], 
+         ["F", "V", "V", "F"],
+         ["F", "F", "V", "V"]]
         
     tabela = [[f"|  {entradaUsuario[0]} ", f"|  {entradaUsuario[2]} ", f"|  ~{entradaUsuario[0]} ", f"|  ~{entradaUsuario[2]}  |"], \
               [f"|  {t[0][0]} ", f"|  {t[0][1]} ", f"|  {t[0][2]}  ", f"|  {t[0][3]}   |"], \
@@ -27,12 +26,33 @@ def implicacao(tabela, entrada):
 
     return implicacao
 
+def bi_implicacao(tabela, entrada):
+    biimplicacao = [f"{entrada}    |", "      V      |","      F      |","      F      |","      V      |"]
+
+    return implicacao
+
+def abc(tabela, entrada):
+    tabc = [f" {entrada}  |", "   V    |","   F    |","   F    |","   F    |"]
+
+    return tabc
+
+def ghi(tabela, entrada):
+    tghi = [f" {entrada}  |", "   V    |","   V    |","   V    |","   F    |"]
+ 
+    return tghi
+
 tabela = imprimeTabela(entradaUsuario)
 
 implicacao = implicacao(tabela, entrada)
 
+tabc = abc(tabela, entrada)
+
+tghi = abc(tabela, entrada)
+
+biImplicacao = bi_implicacao(tabela, entrada)
+
 for i in range(0,5):
-    print( tabela[i][0],tabela[i][1], tabela[i][2], tabela[i][3], implicacao[i]) 
+    print( tabela[i][0],tabela[i][1], tabela[i][2], tabela[i][3], biImplicacao[i]) 
 
 
 
