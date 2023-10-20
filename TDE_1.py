00a414ecfa311ae7f1372b279aec487aac373194
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2023-10-20 17:21:56 -0300

    Melhora tabela verdade

--- TDE_1.py
+++ TDE_1.py
@@ -6,28 +6,26 @@
 # (~p or q )and (~p or q)
 
 
-t = [[True, True, False, False],
-     [True, False, False, True], 
-     [False, True, True, False],
-     [False, False, True, True]]
+t = [["V", "V", "F", "F"],
+     ["V", "F", "F", "V"], 
+     ["F", "V", "V", "F"],
+     ["F", "F", "V", "V"]]
 
 entradaUsuario = "p^q"
     
-def imprimeTabela(t,entradaUsuario):
-    line = "+--------+--------+--------+--------+"
-    cabecalho = 
+def imprimeTabela(t, entradaUsuario):
 
-    tabela = ( f"|  {t[0][0]}  |  {t[0][1]}  | {t[0][2]}  | {t[0][3]}  | \n" \
-               f"|  {t[1][0]}  | {t[1][1]}  | {t[1][2]}  |  {t[1][3]}  | \n" \
-               f"| {t[2][0]}  |  {t[2][1]}  |  {t[2][2]}  | {t[2][3]}  | \n" \
-               f"| {t[3][0]}  | {t[3][1]}  |  {t[3][2]}  |  {t[3][3]}  |" )
+    tabela = ( " +-----+-----+------+------+ \n" \
+               f" |  {entradaUsuario[0]}  |  {entradaUsuario[2]}  |  ~{entradaUsuario[0]}  |  ~{entradaUsuario[2]}  |\n" \
+               " +-----+-----+------+------+ \n " \
+               f"|  {t[0][0]}  |  {t[0][1]}  |   {t[0][2]}  |   {t[0][3]}  | \n" \
+               f" |  {t[1][0]}  |  {t[1][1]}  |   {t[1][2]}  |   {t[1][3]}  | \n" \
+               f" |  {t[2][0]}  |  {t[2][1]}  |   {t[2][2]}  |   {t[2][3]}  | \n" \
+               f" |  {t[3][0]}  |  {t[3][1]}  |   {t[3][2]}  |   {t[3][3]}  | \n" \
+               " +-----+-----+------+------+")
 
-    print(f"{line} \n" \
-          \
-          f"{line} \n" \
-          f"{tabela} \n"
-          f"{line} \n" \
-          )
+    print(tabela)
 
-imprimeTabela(entradaUsuario)
+imprimeTabela(t, entradaUsuario)
+
 
