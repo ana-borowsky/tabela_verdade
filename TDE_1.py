744e2179a63ff5851e0270a304b15d28c4a2be4a
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2023-10-21 18:04:12 -0300

    Adiciona formatação em todas as funções.

--- TDE_1.py
+++ TDE_1.py
@@ -1,138 +1,145 @@
-# p -> q
-# ~p or q
- 
-# p <-> q 
-# p -> q and q -> ~q
-# (~p or q ) and (~p or q)
-
-entrada = "~p <-> q"
-entradaUsuario = entrada.split(" ")
+print("\nBoas vindas!\n")
+entrada = input("Digite sua expressão lógica: ")
+print("\n")
+entradaRecortada = entrada.split(" ")
 tabelaVerdade = [["V", "V", "F", "F"],
                  ["V", "F", "F", "V"], 
                  ["F", "V", "V", "F"],
                  ["F", "F", "V", "V"]]
-primeiraLetra = None
-segundaLetra = None
 
-def pegaLetras(entradaUsuario):
-    if len(entradaUsuario[0]) != 1:
-        primeiraLetra = entradaUsuario[0][1]
+def pegaLetras(entradaRecortada):
+    primeiraLetra = None
+    segundaLetra = None
 
-    else:
-        primeiraLetra = entradaUsuario[0]
-
-    if len(entradaUsuario[2]) != 1:
-        segundaLetra = entradaUsuario[2][1]
+    if len(entradaRecortada[0]) != 1:
+        primeiraLetra = entradaRecortada[0][1]
 
     else:
-        segundaLetra = entradaUsuario[2]
+        primeiraLetra = entradaRecortada[0]
+
+    if len(entradaRecortada[2]) != 1:
+        segundaLetra = entradaRecortada[2][1]
+
+    else:
+        segundaLetra = entradaRecortada[2]
 
     return(primeiraLetra, segundaLetra)
 
-def escolheColuna(entradaUsuario):
+def escolheColuna(entradaRecortada):
     colunaP = 0
     colunaQ = 0
 
-    if '~' in entradaUsuario[0]: 
+    if '~' in entradaRecortada[0]: 
         colunaP = 2
     else:
         colunaP = 0
 
-    if '~' in entradaUsuario[2]: 
+    if '~' in entradaRecortada[2]: 
         colunaQ = 3
     else:
         colunaQ = 1 
     
     return(colunaP, colunaQ)
     
-def formataTabelaVerdade(tabelaVerdade, entradaUsuario):    
+def formataTabelaVerdade(tabelaVerdade, entradaRecortada):    
     t = tabelaVerdade   
-    primeiraLetra = pegaLetras(entradaUsuario)[0]
-    segundaLetra = pegaLetras(entradaUsuario)[1]
+    primeiraLetra = pegaLetras(entradaRecortada)[0]
+    segundaLetra = pegaLetras(entradaRecortada)[1]
 
-    tabelaVerdadeFormatada = [[f"|  {primeiraLetra} ", f"|  {segundaLetra} ", f"|  ~{primeiraLetra} ", f"|  ~{segundaLetra}  |"], \
-                              [f"|  {t[0][0]} ", f"|  {t[0][1]} ", f"|  {t[0][2]}  ", f"|  {t[0][3]}   |"], \
-                              [f"|  {t[1][0]} ", f"|  {t[1][1]} ", f"|  {t[1][2]}  ", f"|  {t[1][3]}   |"], \
-                              [f"|  {t[2][0]} ", f"|  {t[2][1]} ", f"|  {t[2][2]}  ", f"|  {t[2][3]}   |"], \
-                              [f"|  {t[3][0]} ", f"|  {t[3][1]} ", f"|  {t[3][2]}  ", f"|  {t[3][3]}   |"]]
+    tabelaVerdadeFormatada = [[f"  {primeiraLetra} ", f"|  {segundaLetra} ", f"|  ~{primeiraLetra} ", f"|  ~{segundaLetra}  |"], \
+                              [f"  {t[0][0]} ", f"|  {t[0][1]} ", f"|  {t[0][2]}  ", f"|  {t[0][3]}   |"], \
+                              [f"  {t[1][0]} ", f"|  {t[1][1]} ", f"|  {t[1][2]}  ", f"|  {t[1][3]}   |"], \
+                              [f"  {t[2][0]} ", f"|  {t[2][1]} ", f"|  {t[2][2]}  ", f"|  {t[2][3]}   |"], \
+                              [f"  {t[3][0]} ", f"|  {t[3][1]} ", f"|  {t[3][2]}  ", f"|  {t[3][3]}   |"]]
 
     return tabelaVerdadeFormatada
 
-def funcao_e(tabelaVerdade, entradaUsuario):
-    #tabc = [f" {entrada}  |", "   V    |","   F    |","   F    |","   F    |"]
+def funcaoE(tabelaVerdade, entrada, entradaRecortada):
+    valoresLogicos = [None]*4
     t = tabelaVerdade
-    valores_logicos = [None]*4
 
-    colunaP = escolheColuna(entradaUsuario)[0]
-    colunaQ = escolheColuna(entradaUsuario)[1]
+    colunaP = escolheColuna(entradaRecortada)[0]
+    colunaQ = escolheColuna(entradaRecortada)[1]
 
     for i in range(0,4):
         if t[i][colunaP] == "V" and t[i][colunaQ] == "V":
-            valores_logicos[i] = "V"
+            valoresLogicos[i] = "V"
         else:
-            valores_logicos[i] = "F"  
+            valoresLogicos[i] = "F"  
+    
+    valoresLogicosFormatada = [f" {entrada}", \
+                               f"   {valoresLogicos[0]}", \
+                               f"   {valoresLogicos[1]}", \
+                               f"   {valoresLogicos[2]}", \
+                               f"   {valoresLogicos[3]}"]
 
-    return valores_logicos
+    return valoresLogicosFormatada
 
-def funcao_ou(tabelaVerdade, entrada):
-    # tghi = [f" {entrada}  |", "   V    |","   V    |","   V    |","   F    |"]
+def funcaoOu(tabelaVerdade, entrada):
     t = tabelaVerdade
-    valores_logicos = [None]*4
-    colunaP = escolheColuna(entradaUsuario)[0]
-    colunaQ = escolheColuna(entradaUsuario)[1]
+    valoresLogicos = [None]*4
+    colunaP = escolheColuna(entradaRecortada)[0]
+    colunaQ = escolheColuna(entradaRecortada)[1]
 
     for i in range(0,4):
         if t[i][colunaP] == "V" or t[i][colunaQ] == "V":
-            valores_logicos[i] = "V"
+            valoresLogicos[i] = "V"
         else:
-            valores_logicos[i] = "F"  
+            valoresLogicos[i] = "F"  
  
-    return valores_logicos
+    valoresLogicosFormatada = [f" {entrada}", \
+                               f"   {valoresLogicos[0]}", \
+                               f"   {valoresLogicos[1]}", \
+                               f"   {valoresLogicos[2]}", \
+                               f"   {valoresLogicos[3]}"]
 
-def funcao_implicacao(tabelaVerdade, entrada):
-    #implicacao = [f"  {entrada}  |", "    V     |","    F     |","    V     |","    V     |"]
+    return valoresLogicosFormatada
+
+def funcaoImplicacao(tabelaVerdade, entrada):
     t = tabelaVerdade
-    valores_logicos = [None]*4
-    colunaP = escolheColuna(entradaUsuario)[0]
-    colunaQ = escolheColuna(entradaUsuario)[1]
+    valoresLogicos = [None]*4
+    colunaP = escolheColuna(entradaRecortada)[0]
+    colunaQ = escolheColuna(entradaRecortada)[1]
 
     for i in range(0,4):
         if t[i][colunaP] == "V" and t[i][colunaQ] == "F":
-            valores_logicos[i] = "F"
+            valoresLogicos[i] = "F"
         else:
-            valores_logicos[i] = "V"  
+            valoresLogicos[i] = "V"  
 
-    return valores_logicos
+    valoresLogicosFormatada = [f" {entrada}", \
+                               f"   {valoresLogicos[0]}", \
+                               f"   {valoresLogicos[1]}", \
+                               f"   {valoresLogicos[2]}", \
+                               f"   {valoresLogicos[3]}"]
 
-def funcao_bi_implicacao(tabelaVerdade, entrada):
-    #biimplicacao = [f"{entrada}    |", "      V      |","      F      |","      F      |","      V      |"]
+    return valoresLogicosFormatada
+
+def funcaoBiImplicacao(tabelaVerdade, entrada):
     t = tabelaVerdade
-    valores_logicos = [None]*4
-    colunaP = escolheColuna(entradaUsuario)[0]
-    colunaQ = escolheColuna(entradaUsuario)[1]
+    valoresLogicos = [None]*4
+    colunaP = escolheColuna(entradaRecortada)[0]
+    colunaQ = escolheColuna(entradaRecortada)[1]
 
     for i in range(0,4):
         if t[i][colunaP] != t[i][colunaQ]:
-            valores_logicos[i] = "F"
+            valoresLogicos[i] = "F"
         else:
-            valores_logicos[i] = "V"  
+            valoresLogicos[i] = "V"  
 
-    return valores_logicos
+    valoresLogicosFormatada = [f" {entrada}", \
+                               f"   {valoresLogicos[0]}", \
+                               f"   {valoresLogicos[1]}", \
+                               f"   {valoresLogicos[2]}", \
+                               f"   {valoresLogicos[3]}"]
 
+    return valoresLogicosFormatada
 
-
-# tabela = imprimeTabela(entradaUsuario)
-
-# implicacao = implicacao(tabela, entrada)
-
-# tabc = abc(tabela, entrada)
-
-# tghi = abc(tabela, entrada)
-tabela = formataTabelaVerdade(tabelaVerdade, entradaUsuario)
-biImplicacao = funcao_bi_implicacao(tabelaVerdade, entrada)
+tabela = formataTabelaVerdade(tabelaVerdade, entradaRecortada)
+teste = funcaoE(tabelaVerdade, entrada, entradaRecortada)
 
 for i in range(0,5):
-    print( tabela[i][0],tabela[i][1], tabela[i][2], tabela[i][3]) 
+    print( tabela[i][0],tabela[i][1], tabela[i][2], tabela[i][3], teste[i]) 
 
 #print(tabela)
 
