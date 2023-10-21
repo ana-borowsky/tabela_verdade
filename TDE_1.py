24c92db2eb4fcdb177976e31f38fe44962ec900a
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2023-10-21 17:35:52 -0300

    Adiciona funções

--- TDE_1.py
+++ TDE_1.py
@@ -5,17 +5,52 @@
 # p -> q and q -> ~q
 # (~p or q ) and (~p or q)
 
-entrada = "p <-> q"
+entrada = "~p <-> q"
 entradaUsuario = entrada.split(" ")
 tabelaVerdade = [["V", "V", "F", "F"],
                  ["V", "F", "F", "V"], 
                  ["F", "V", "V", "F"],
                  ["F", "F", "V", "V"]]
+primeiraLetra = None
+segundaLetra = None
+
+def pegaLetras(entradaUsuario):
+    if len(entradaUsuario[0]) != 1:
+        primeiraLetra = entradaUsuario[0][1]
+
+    else:
+        primeiraLetra = entradaUsuario[0]
+
+    if len(entradaUsuario[2]) != 1:
+        segundaLetra = entradaUsuario[2][1]
+
+    else:
+        segundaLetra = entradaUsuario[2]
+
+    return(primeiraLetra, segundaLetra)
+
+def escolheColuna(entradaUsuario):
+    colunaP = 0
+    colunaQ = 0
+
+    if '~' in entradaUsuario[0]: 
+        colunaP = 2
+    else:
+        colunaP = 0
+
+    if '~' in entradaUsuario[2]: 
+        colunaQ = 3
+    else:
+        colunaQ = 1 
+    
+    return(colunaP, colunaQ)
     
 def formataTabelaVerdade(tabelaVerdade, entradaUsuario):    
     t = tabelaVerdade   
+    primeiraLetra = pegaLetras(entradaUsuario)[0]
+    segundaLetra = pegaLetras(entradaUsuario)[1]
 
-    tabelaVerdadeFormatada = [[f"|  {entradaUsuario[0]} ", f"|  {entradaUsuario[2]} ", f"|  ~{entradaUsuario[0]} ", f"|  ~{entradaUsuario[2]}  |"], \
+    tabelaVerdadeFormatada = [[f"|  {primeiraLetra} ", f"|  {segundaLetra} ", f"|  ~{primeiraLetra} ", f"|  ~{segundaLetra}  |"], \
                               [f"|  {t[0][0]} ", f"|  {t[0][1]} ", f"|  {t[0][2]}  ", f"|  {t[0][3]}   |"], \
                               [f"|  {t[1][0]} ", f"|  {t[1][1]} ", f"|  {t[1][2]}  ", f"|  {t[1][3]}   |"], \
                               [f"|  {t[2][0]} ", f"|  {t[2][1]} ", f"|  {t[2][2]}  ", f"|  {t[2][3]}   |"], \
@@ -28,8 +63,11 @@ def funcao_e(tabelaVerdade, entradaUsuario):
     t = tabelaVerdade
     valores_logicos = [None]*4
 
+    colunaP = escolheColuna(entradaUsuario)[0]
+    colunaQ = escolheColuna(entradaUsuario)[1]
+
     for i in range(0,4):
-        if t[i][0] == "V" and t[i][1] == "V":
+        if t[i][colunaP] == "V" and t[i][colunaQ] == "V":
             valores_logicos[i] = "V"
         else:
             valores_logicos[i] = "F"  
@@ -40,9 +78,11 @@ def funcao_ou(tabelaVerdade, entrada):
     # tghi = [f" {entrada}  |", "   V    |","   V    |","   V    |","   F    |"]
     t = tabelaVerdade
     valores_logicos = [None]*4
+    colunaP = escolheColuna(entradaUsuario)[0]
+    colunaQ = escolheColuna(entradaUsuario)[1]
 
     for i in range(0,4):
-        if t[i][0] == "V" or t[i][1] == "V":
+        if t[i][colunaP] == "V" or t[i][colunaQ] == "V":
             valores_logicos[i] = "V"
         else:
             valores_logicos[i] = "F"  
@@ -53,9 +93,11 @@ def funcao_implicacao(tabelaVerdade, entrada):
     #implicacao = [f"  {entrada}  |", "    V     |","    F     |","    V     |","    V     |"]
     t = tabelaVerdade
     valores_logicos = [None]*4
+    colunaP = escolheColuna(entradaUsuario)[0]
+    colunaQ = escolheColuna(entradaUsuario)[1]
 
     for i in range(0,4):
-        if t[i][0] == "V" and t[i][1] == "F":
+        if t[i][colunaP] == "V" and t[i][colunaQ] == "F":
             valores_logicos[i] = "F"
         else:
             valores_logicos[i] = "V"  
@@ -66,9 +108,11 @@ def funcao_bi_implicacao(tabelaVerdade, entrada):
     #biimplicacao = [f"{entrada}    |", "      V      |","      F      |","      F      |","      V      |"]
     t = tabelaVerdade
     valores_logicos = [None]*4
+    colunaP = escolheColuna(entradaUsuario)[0]
+    colunaQ = escolheColuna(entradaUsuario)[1]
 
     for i in range(0,4):
-        if t[i][0] != t[i][1]:
+        if t[i][colunaP] != t[i][colunaQ]:
             valores_logicos[i] = "F"
         else:
             valores_logicos[i] = "V"  
@@ -84,12 +128,20 @@ def funcao_bi_implicacao(tabelaVerdade, entrada):
 # tabc = abc(tabela, entrada)
 
 # tghi = abc(tabela, entrada)
+tabela = formataTabelaVerdade(tabelaVerdade, entradaUsuario)
+biImplicacao = funcao_bi_implicacao(tabelaVerdade, entrada)
 
-# biImplicacao = bi_implicacao(tabela, entrada)
+for i in range(0,5):
+    print( tabela[i][0],tabela[i][1], tabela[i][2], tabela[i][3]) 
 
-# for i in range(0,5):
-#     print( tabela[i][0],tabela[i][1], tabela[i][2], tabela[i][3], biImplicacao[i]) 
-print(funcao_bi_implicacao(tabelaVerdade, entrada))
+#print(tabela)
+
+
+
+
+
+
+
 
 
 
